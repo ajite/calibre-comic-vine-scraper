6aea5fcd14f331814e3cad755e39d05eb9e56055
Augustin Cisterne-Kaas <ajitekun@gmail.com>
2024-03-10 21:45:42 +0800

    feat: use books without series when no matching series are found
    Books with name = None won't crash
    Books with volumer number in the author name can be sorted without renaming the books

--- update_calibre.py
+++ update_calibre.py
@@ -1,4 +1,5 @@
 import calibre.library
+from calibre.ebooks.metadata import title_sort, authors_to_sort_string
 import re
 import json
 import datetime
@@ -30,6 +31,17 @@ def get_all_books_by_series(series_name: str):
     for book_id in book_ids:
         book = calibre_db.get_metadata(book_id)
         if book.series == series_name:
+            books.append(book)
+    return books
+
+
+def get_all_books_without_series():
+    """Get all books in the Calibre library without a series."""
+    book_ids = calibre_db.all_book_ids()
+    books = []
+    for book_id in book_ids:
+        book = calibre_db.get_metadata(book_id)
+        if not book.series:
             books.append(book)
     return books
 
@@ -77,6 +89,7 @@ def calibre_update_metadata(book, issue, issue_nb_padding=2):
     """Update the metadata of the book in the Calibre library."""
     mi = calibre_db.get_metadata(book.id)
     black_list_titles = build_black_list_titles(issue)
+    issue["name"] = issue["name"] or ""
     if issue["name"].lower().replace(" ", "") in black_list_titles:
         mi.title = "{} - Vol. {}".format(
             issue["volume"], str(issue["issue_number"]).zfill(issue_nb_padding)
@@ -93,6 +106,12 @@ def calibre_update_metadata(book, issue, issue_nb_padding=2):
     mi.authors = issue["person_credits"]
     mi.pubdate = datetime.datetime.strptime(issue["cover_date"], "%Y-%m-%d")
     mi.publisher = issue["publisher"]
+
+    # Sort title
+    mi.title_sort = title_sort(mi.title)
+    # Sort author(s)
+    mi.author_sort = authors_to_sort_string(mi.authors)
+
     calibre_db.set_metadata(book.id, mi)
 
     print("Updated metadata for book: {}".format(book.title))
@@ -105,6 +124,15 @@ if __name__ == "__main__":
         exit(1)
     search_term = results[0]["volume"]
     books = get_all_books_by_series(search_term)
+    if len(books) == 0:
+        print("No books found for series: {}".format(search_term))
+        books = get_all_books_without_series()
+        print("Found {} books without series".format(len(books)))
+        if len(books) == 0:
+            print("No books found without series")
+            exit(1)
+    else:
+        print("Found {} books for series: {}".format(len(books), search_term))
     serie_index_results = {}
     for result in results:
         serie_index_results[int(result["issue_number"])] = result
@@ -114,6 +142,9 @@ if __name__ == "__main__":
         serie_index = get_serie_index_from_title(book.title)
         if serie_index is None and book.series_index:
             serie_index = int(book.series_index)
+        # Sometimes the volume number is in the author field
+        if serie_index is None and book.authors:
+            serie_index = get_serie_index_from_title(book.authors[0])
 
         if serie_index is not None:
             print(book.title + " - " + str(serie_index))
